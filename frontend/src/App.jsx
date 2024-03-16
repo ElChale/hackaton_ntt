adead871a25634f0ba5858371172ae92a4708bba
Chale <charlie.feijoo@gmail.com>
2024-03-15 19:09:31 -0500

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import { Container, Row, Col, Form, FormControl, Card, Dropdown, Badge, Modal, Button, OverlayTrigger, Tooltip, ToggleButton, ToggleButtonGroup } from 'react-bootstrap';
+import viteLogo from '/vite.svg'
+
+function App() {
+      const [count, setCount] = useState(0)
+
+      return (
+            <Container>
+                  <Row>
+                        <Col>
+                              xd
+                        </Col>
+                  </Row>
+            </Container>
+      )
+}
+
+export default App
